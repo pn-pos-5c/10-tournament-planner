195baf36a766b307600df00e0d460da939b8d1bd
Philipp <philipp.niemetz@gmail.com>
2021-10-13 11:35:14 +0200

    update

---
+++ TournamentPlanner/TournamentDb/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentDb
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public Gender Gender { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
